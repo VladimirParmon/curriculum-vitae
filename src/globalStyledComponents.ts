6cadfe153813e3981419c3f16b85098aff4576f7
VladimirParmon <parmon_vladimir@bk.ru>
2022-12-04 23:47:25 +0300

    add containers' global styles

---
+++ src/globalStyledComponents.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { bp } from "./constants/breakpoints";
+
+export const MainContainer = styled.div`
+  background-image: url("${(props) => props.theme.bgImage}");
+  background-size: contain;
+  position: relative;
+  font-size: 1.5rem;
+  line-height: 2.8rem;
+  padding-top: 10px;
+  ${bp.maxWidth("m")`
+    font-size: 1rem;
+    line-height: 1.8rem;
+  `}
+`;
+
+export const ContentContainer = styled.div`
+  position: relative;
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 10px 30px 20px 30px;
+  border-radius: 50px;
+  background-color: ${(props) => props.theme.background};
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: 400;
+  color: ${(props) => props.theme.color};
+`;
+
+export const PageControls = styled.div`
+  display: flex;
+  width: fit-content;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
