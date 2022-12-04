89cb671a7b8239be992fa76c31b48eda5360611c
VladimirParmon <parmon_vladimir@bk.ru>
2022-12-04 23:44:17 +0300

    add slides array

---
+++ src/constants/slides.ts
@@ -0,0 +1,74 @@
+export const slides = [
+  {
+    slideName: "Museum",
+    link: "https://rolling-scopes-school.github.io/vladimirparmon-JSFE2021Q3/Museum/",
+    techLogos: ["assets/slider/techs/javascript-original.svg"],
+    imageSrc: "assets/slider/museum.JPG",
+  },
+  {
+    slideName: "RS Lang",
+    link: "https://vladimirparmon.github.io/RS-Lang/index.html",
+    techLogos: ["assets/slider/techs/typescript-original.svg"],
+    imageSrc: "assets/slider/lang.JPG",
+  },
+  {
+    slideName: "Hotel",
+    link: "https://github.com/VladimirParmon/Hotel",
+    techLogos: ["assets/slider/techs/react-original.svg"],
+    imageSrc: "assets/slider/hotel.JPG",
+  },
+  {
+    slideName: "Builder project",
+    link: "https://github.com/VladimirParmon/BuilderProjectV2",
+    techLogos: ["assets/slider/techs/angularjs-original.svg", "assets/electron-original.svg"],
+    imageSrc: "assets/slider/builder.JPG",
+  },
+  {
+    slideName: "Project manager",
+    link: "https://github.com/VladimirParmon/project-managing-app/tree/develop",
+    techLogos: ["assets/slider/techs/angularjs-original.svg"],
+    imageSrc: "assets/slider/PMA.jpg",
+  },
+  {
+    slideName: "NodeJS file manager",
+    link: "https://github.com/VladimirParmon/Node.js-file-manager",
+    techLogos: [],
+    imageSrc: "assets/slider/nodejs.png",
+  },
+  {
+    slideName: "NodeJS CRUD API",
+    link: "https://github.com/VladimirParmon/node-js-crud",
+    techLogos: [],
+    imageSrc: "assets/slider/nodejs2.png",
+  },
+  {
+    slideName: "Christmas app",
+    link: "https://rolling-scopes-school.github.io/vladimirparmon-JSFE2021Q3/Christmas-task/",
+    techLogos: ["assets/slider/techs/typescript-original.svg"],
+    imageSrc: "assets/slider/Christmas.JPG",
+  },
+  {
+    slideName: "Calculator",
+    link: "https://github.com/VladimirParmon/calculator",
+    techLogos: ["assets/slider/techs/react-original.svg"],
+    imageSrc: "assets/slider/calculator.jpg",
+  },
+  {
+    slideName: "Momentum",
+    link: "https://rolling-scopes-school.github.io/vladimirparmon-JSFE2021Q3/Momentum/",
+    techLogos: ["assets/slider/techs/javascript-original.svg"],
+    imageSrc: "assets/slider/momentum.JPG",
+  },
+  {
+    slideName: "Art quiz",
+    link: "https://rolling-scopes-school.github.io/vladimirparmon-JSFE2021Q3/App/",
+    techLogos: ["assets/slider/techs/javascript-original.svg"],
+    imageSrc: "assets/slider/art-quiz.JPG",
+  },
+  {
+    slideName: "Drum kit",
+    link: "https://rolling-scopes-school.github.io/vladimirparmon-JSFEPRESCHOOL/Drumkit/Index.html",
+    techLogos: ["assets/slider/techs/javascript-original.svg"],
+    imageSrc: "assets/slider/drumkit.jpg",
+  },
+];
