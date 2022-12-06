9f6836f6d08406876f199d2e7623ee96613f9be4
VladimirParmon <parmon_vladimir@bk.ru>
2022-12-07 01:20:17 +0300

    export breakpoints values

--- src/constants/breakpoints.ts
+++ src/constants/breakpoints.ts
@@ -1,10 +1,12 @@
 import breakpoints from "styled-components-breakpoints";
 
-export const bp = breakpoints({
+export const myBreakpoints = {
   xxs: 0,
   xs: 320,
   s: 536,
   m: 768,
   l: 992,
   xl: 1250,
-});
+};
+
+export const bp = breakpoints(myBreakpoints);
