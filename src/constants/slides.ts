003cf53f97e1160fb183f65eb1944bae3a149431
VladimirParmon <parmon_vladimir@bk.ru>
2022-12-07 22:42:38 +0300

    fix image file extensions

--- src/constants/slides.ts
+++ src/constants/slides.ts
@@ -5,19 +5,19 @@ export const slides: Slide[] = [
     slideName: "Museum",
     link: "https://rolling-scopes-school.github.io/vladimirparmon-JSFE2021Q3/Museum/",
     techLogos: ["assets/slider/techs/javascript-original.svg"],
-    imageSrc: "assets/slider/museum.JPG",
+    imageSrc: "assets/slider/museum.jpg",
   },
   {
     slideName: "RS Lang",
     link: "https://vladimirparmon.github.io/RS-Lang/index.html",
     techLogos: ["assets/slider/techs/typescript-original.svg"],
-    imageSrc: "assets/slider/lang.JPG",
+    imageSrc: "assets/slider/lang.jpg",
   },
   {
     slideName: "Hotel",
     link: "https://github.com/VladimirParmon/Hotel",
     techLogos: ["assets/slider/techs/react-original.svg"],
-    imageSrc: "assets/slider/hotel.JPG",
+    imageSrc: "assets/slider/hotel.jpg",
   },
   {
     slideName: "Builder project",
@@ -26,7 +26,7 @@ export const slides: Slide[] = [
       "assets/slider/techs/angularjs-original.svg",
       "assets/slider/techs/electron-original.svg",
     ],
-    imageSrc: "assets/slider/builder.JPG",
+    imageSrc: "assets/slider/builder.jpg",
   },
   {
     slideName: "Project manager",
@@ -50,7 +50,7 @@ export const slides: Slide[] = [
     slideName: "Christmas app",
     link: "https://rolling-scopes-school.github.io/vladimirparmon-JSFE2021Q3/Christmas-task/",
     techLogos: ["assets/slider/techs/typescript-original.svg"],
-    imageSrc: "assets/slider/Christmas.JPG",
+    imageSrc: "assets/slider/christmas.jpg",
   },
   {
     slideName: "Calculator",
@@ -62,13 +62,13 @@ export const slides: Slide[] = [
     slideName: "Momentum",
     link: "https://rolling-scopes-school.github.io/vladimirparmon-JSFE2021Q3/Momentum/",
     techLogos: ["assets/slider/techs/javascript-original.svg"],
-    imageSrc: "assets/slider/momentum.JPG",
+    imageSrc: "assets/slider/momentum.jpg",
   },
   {
     slideName: "Art quiz",
     link: "https://rolling-scopes-school.github.io/vladimirparmon-JSFE2021Q3/App/",
     techLogos: ["assets/slider/techs/javascript-original.svg"],
-    imageSrc: "assets/slider/art-quiz.JPG",
+    imageSrc: "assets/slider/art-quiz.jpg",
   },
   {
     slideName: "Drum kit",
